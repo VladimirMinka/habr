494c4b7f4a22109ff2b6c8ce4ddd38da8eea295f
Vladimir Minka <38921290+VladimirMinka@users.noreply.github.com>
2025-08-11 12:11:11 +0300

    испарвил замечания?создал два теста и два метода.

--- src/test/java/ru/vladimirminka/habr/MainPageTest.java
+++ src/test/java/ru/vladimirminka/habr/MainPageTest.java
@@ -24,27 +24,32 @@ public class MainPageTest {
         driver.get("https://www.habr.com/");
 
     }
-
     @Test
-    public void searchBtn() {
-        // тест 1
-        WebElement searchBox = driver.findElement(By.cssSelector("a.tm-header__become-author-btn"));
-        searchBox.click();
-        assertTrue(searchBox.isDisplayed(), "кнопки нет");
-        // тест 2
-        List<WebElement> btnIsEmpty = driver.findElements(By.xpath("//*[contains(text(),'Образовательные программы')]"));
-
-        assertFalse(btnIsEmpty.isEmpty(), "Образовательные программы не найдены");
-        /*
+    @DisplayName("Проверка кнопки 'Как стать автором'")
+    public void testBecomeAuthorButton() {
+          /*
         Описание Теста 1:
         Открытие начальной страницы- "https://habr.com/ru/")
         Ищем кнопку "Как стать автором", кнопка имеет аттрибут - .tm-header__become-author-btn
         При  успешном поиске кликаем на кнопку и выводим  сообщение тест пройден
+         */
+        WebElement searchBox = driver.findElement(By.cssSelector("a.tm-header__become-author-btn"));
+        searchBox.click();
+        assertTrue(searchBox.isDisplayed(), "кнопки нет");
+    }
 
+    @Test
+    @DisplayName("Проверка наличия текста 'Образовательные программы'")
+    /*
         Описание Теста 2:
         Необходимо проверить наличие элемента страницы с текстом "Образовательные программы" (с учётом пробелов).
         В случае если элемент не найден выводим сообщение.
-         */
+     */
+
+    public void testEducationalProgramsText() {
+        List<WebElement> btnIsEmpty = driver.findElements(By.xpath("//*[contains(text()," +
+                "'Образовательные программы')]"));
+        assertFalse(btnIsEmpty.isEmpty(), "Образовательные программы не найдены");
 
     }
 }
